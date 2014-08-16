c0824ed11b5957c0124c94902a8932b3d8ebefce
Khaled alHabache <khellls@gmail.com>
2014-08-16 16:39:14 +0300

    update coverage metric

--- spec/helper.rb
+++ spec/helper.rb
@@ -9,7 +9,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
 
 SimpleCov.start do
   add_filter '/spec/'
-  minimum_coverage(99.15)
+  minimum_coverage(95)
 end
 
 require_relative '../lib/rich_email_validator'
